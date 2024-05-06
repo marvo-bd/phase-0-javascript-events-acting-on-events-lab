fc6ab0dec720dc8fa3965746d7cf9da43e5727dd
Marvin Kabira <marvobd@gmail.com>
2024-05-06 19:12:58 +0300

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Your code here
+let dodger = document.getElementById('dodger');
+
+function moveDodgerLeft() {
+  let leftPosition = parseInt(dodger.style.left) || 0;
+  if (leftPosition > 0) {
+    dodger.style.left = `${leftPosition - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+  let leftPosition = parseInt(dodger.style.left) || 0;
+  if (leftPosition < 360) {
+    dodger.style.left = `${leftPosition + 1}px`;
+  }
+}
